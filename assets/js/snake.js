c3bf834052269eca3d3dedecab0260e6839cf3bd
Yash Bharatula <ybharatu@purdue.edu>
2022-11-08 19:28:35 -0500

    Copied some code

---
+++ assets/js/snake.js
@@ -0,0 +1,82 @@
+
+const board_border = 'black';
+const board_background = "white";
+const snake_col = 'lightblue';
+const snake_border = 'darkblue';
+
+let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
+
+// Horizontal velocity
+let dx = 10;
+// Vertical velocity
+let dy = 0;
+
+// Get the canvas element
+const snakeboard = document.getElementById("snakeboard");
+// Return a two dimensional drawing context
+const snakeboard_ctx = snakeboard.getContext("2d");
+// Start game
+main();
+
+// main function called repeatedly to keep the game running
+function main() {
+    setTimeout(function onTick() {
+    clear_board();
+    move_snake();
+    drawSnake();
+    // Call main again
+    main();
+  }, 100)
+}
+
+// draw a border around the canvas
+function clearCanvas() {
+  //  Select the colour to fill the drawing
+  snakeboard_ctx.fillStyle = board_background;
+  //  Select the colour for the border of the canvas
+  snakeboard_ctx.strokestyle = board_border;
+  // Draw a "filled" rectangle to cover the entire canvas
+  snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
+  // Draw a "border" around the entire canvas
+  snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
+}
+
+// draw a border around the canvas
+function clear_board() {
+  //  Select the colour to fill the drawing
+  snakeboard_ctx.fillStyle = board_background;
+  //  Select the colour for the border of the canvas
+  snakeboard_ctx.strokestyle = board_border;
+  // Draw a "filled" rectangle to cover the entire canvas
+  snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
+  // Draw a "border" around the entire canvas
+  snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
+}
+
+// Draw the snake on the canvas
+function drawSnake() {
+  // Draw each part
+  snake.forEach(drawSnakePart)
+}
+
+// Draw one snake part
+function drawSnakePart(snakePart) {
+
+  // Set the colour of the snake part
+  snakeboard_ctx.fillStyle = snake_col;
+  // Set the border colour of the snake part
+  snakeboard_ctx.strokestyle = snake_border;
+  // Draw a "filled" rectangle to represent the snake part at the coordinates
+  // the part is located
+  snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  // Draw a border around the snake part
+  snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function move_snake() {
+  // Create the new Snake's head
+  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+  // Add the new head to the beginning of snake body
+  snake.unshift(head);
+  snake.pop();
+}
