644eb2b8d3db207cc43e8711637d5be51e5cdb44
Yash Bharatula <ybharatu@purdue.edu>
2022-11-08 19:05:18 -0500

    Added Base files

---
+++ assets/js/scripts.js
@@ -0,0 +1,74 @@
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+$(function() {
+  $(".portfolio-box").slice(0, 3).show(); // select the first 3
+  $("#load").click(function(e) { // click event for load more
+    e.preventDefault();
+    $(".portfolio-box:hidden").slice(0, 3).show(); // select next 3 hidden divs and show them
+    if ($(".portfolio-box:hidden").length == 0) { // check if any hidden divs still exist
+      alert("No more divs"); // alert if there are none left
+    }
+  });
+});
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+	
+    /*
+        Background slideshow
+    */
+    $('.top-content').backstretch("/img/backgrounds/1.jpg");
+    $('.call-to-action-container').backstretch("/img/backgrounds/1.jpg");
+    $('.testimonials-container').backstretch("/img/backgrounds/1.jpg");
+    
+    $('#top-navbar-1').on('shown.bs.collapse', function(){
+    	$('.top-content').backstretch("resize");
+    });
+    $('#top-navbar-1').on('hidden.bs.collapse', function(){
+    	$('.top-content').backstretch("resize");
+    });
+    
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(){
+    	$('.testimonials-container').backstretch("resize");
+    });
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+	
+});
+
+
+jQuery(window).load(function() {
+	
+	/*
+		Hidden images
+	*/
+	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
+	
+});
+
